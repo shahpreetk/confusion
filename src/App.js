eb1472730cba92e86b289309b3ec52f7d18a078f
Preet Shah <shahpreetk@gmail.com>
2020-08-26 09:55:25 +0530

    displaying menucompomnent with cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Menu from './components/MenuComponents';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+    return (
+      <Menu dishes={this.state.dishes} />
+    )
+  }
 }
 
 export default App;
